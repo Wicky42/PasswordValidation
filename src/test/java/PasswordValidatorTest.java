06e26c5ce7be50e89a8e0e203b285ca4cd51c3ad
Viktoria Uyanik <viktoria.uyanik@outlook.de>
2026-02-26 14:23:10 +0100

    test add hasMinLength_shouldReturnTrue_whenStringHasMinLength curent red

--- src/test/java/PasswordValidatorTest.java
+++ src/test/java/PasswordValidatorTest.java
@@ -1,11 +1,19 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordValidatorTest {
 
-    @Test
-    void hasMinLength() {
+    @ParameterizedTest
+    @CsvSource({
+            "TestPasswort23, 8",
+            "Lkksmdnr81+, 8",
+            "pldm=82WtH, 4"
+    })
+    void hasMinLength_shouldReturnTrue_whenStringHasMinLength(String text, int min) {
+        assertTrue(PasswordValidator.hasMinLength(text, min));
     }
 
     @Test
