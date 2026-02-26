2b48a61175de79a68ce93a7b2a9d2f4dcd400214
Viktoria Uyanik <viktoria.uyanik@outlook.de>
2026-02-26 14:18:49 +0100

    add PasswordValidator Test Class and pom dependecy Junit

---
+++ src/test/java/PasswordValidatorTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordValidatorTest {
+
+    @Test
+    void hasMinLength() {
+    }
+
+    @Test
+    void containsDigit() {
+    }
+
+    @Test
+    void containsUpperAndLower() {
+    }
+
+    @Test
+    void isCommonPassword() {
+    }
+
+    @Test
+    void containsSpecialChar() {
+    }
+
+    @Test
+    void isValid() {
+    }
+}
