99a0a3b432a0f854c78cf838c797851dd0cb0054
Viktoria Uyanik <viktoria.uyanik@outlook.de>
2026-02-26 14:16:01 +0100

    add PasswordValidator Class with methodsignatures only

---
+++ src/main/java/PasswordValidator.java
@@ -0,0 +1,31 @@
+public final class PasswordValidator {
+
+    public static boolean hasMinLength(String password, int min) {
+        return false;
+    }
+
+    public static boolean containsDigit(String password) {
+        return false;
+    }
+
+    public static boolean containsUpperAndLower(String password) {
+        return false;
+    }
+
+    public static boolean isCommonPassword(String password) // kleine interne Liste
+    {
+        return false;
+    }
+
+    // Bonus:
+    public static boolean containsSpecialChar(String password, String allowed) {
+        return false;
+    }
+
+    // Optionale Gesamtsicht:
+    public static boolean isValid(String password) // nutzt die obenstehenden Checks
+    {
+        return false;
+    }
+
+}
