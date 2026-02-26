3f9ae3968ba49a7f674d0d03d52fe10acea467a3
Viktoria Uyanik <viktoria.uyanik@outlook.de>
2026-02-26 16:30:50 +0100

    add main function. user can type in password

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
 
-    void main(){
-        System.out.println("Hello World!");
+    static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        String password;
+
+        System.out.println("Hello, please set a password.");
+        do{
+            getText();
+            password = scanner.nextLine();
+        }while(!PasswordValidator.isValid(password));
+
+        System.out.println("Thank, password is set");
+        System.out.println("Your password is: " + password);
     }
+
+    public static void getText(){
+        System.out.println("""
+        Password requirements:
+        - At least 8 characters long
+        - At least one uppercase letter
+        - At least one lowercase letter
+        - At least one digit
+        - At least one special character
+        >""");    }
 
 
 
