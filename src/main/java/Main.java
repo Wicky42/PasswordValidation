950968935a21b72b854747c43d01fcf699b7f775
Viktoria Uyanik <viktoria.uyanik@outlook.de>
2026-02-27 11:02:07 +0100

    update user can leave without entering a valid password

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,17 +4,30 @@ public class Main {
 
     static void main(String[] args){
 
+        final String SUCCSESS_MESSAGE = "Danke, ihr Passwort wurde gespeichert.";
+        final String EXIT_Message = "Es wurde kein Passwort gespeichert.";
+
         Scanner scanner = new Scanner(System.in);
         String password;
 
         System.out.println("Hello, please set a password.");
+        getText();
         do{
-            getText();
             password = scanner.nextLine();
+            if(password.equals("q")){
+                break;
+            }
+            try{
+                PasswordValidator.validateOrThrow(password);
+            }
+            catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+                System.out.println("q for exit");
+            }
         }while(!PasswordValidator.isValid(password));
 
-        System.out.println("Thank, password is set");
-        System.out.println("Your password is: " + password);
+        String message = PasswordValidator.isValid(password) ? SUCCSESS_MESSAGE : EXIT_Message;
+        System.out.println(message);
     }
 
     public static void getText(){
@@ -25,8 +38,9 @@ public class Main {
         - At least one lowercase letter
         - At least one digit
         - At least one special character
-        >""");    }
-
-
+        
+        - q for exit
+        >""");
+    }
 
 }
